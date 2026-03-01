78438f61ab7d7276318423bfca8cbd05b08a8612
suhail7868 <63796357+suhail7868@users.noreply.github.com>
2026-03-01 17:32:08 +0000

    app.py is now a flask app

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template, request
+app=Flask(__name__)
+@app.route('/') 
+def home():
+    return "Hello World"
+@app.route('/new')
+def new():
+    return "This is a new page"
 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
